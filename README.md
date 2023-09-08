542c08527493f9a7ca4945a2d34374c6bc986000
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2023-09-08 14:02:55 +0200

    aggiunte tutte le ultime funzionalità. Light/dark mode e readme file

---
+++ README.md
@@ -0,0 +1,33 @@
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/github_username/repo_name">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">Counter.js</h3>
+
+  <p align="center">
+   Start2Impact Project
+
+  </p>
+</div>
+
+
+<!-- About the project -->
+## About the project
+
+This is my counter build for the javascript basic module of Start2Impact. 
+
+### Idea
+
+I wanted to create a beatiful and a bit more fun counter. You will find a dark/light mode and some emojis to increment the count.
+
+### Link
+
+To watch the project open this link:
+https://stupendous-gelato-9e0791.netlify.app
+
--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@
 let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
+const off = document.querySelector('.left');
+const on = document.querySelector('.right');
+
 
 document.querySelector('#value').innerHTML = count;
-document.querySelector('.decrease').innerHTML = '-';
-document.querySelector('.increase').innerHTML = '+';
+document.querySelector('.decrease').innerHTML = '👈';
+document.querySelector('.increase').innerHTML = '👉';
 
 
 btns.forEach(function(btn){
@@ -18,4 +21,17 @@ count--;
 }
 value.textContent = count;
 });
+});
+
+
+//dark mode
+
+off.addEventListener('click', function(){
+  let element = document.body;
+  element.className = 'light__mode'
+});
+
+on.addEventListener('click', function(){
+  let element = document.body;
+  element.className = 'dark__mode'
 });
